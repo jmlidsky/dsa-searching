ca075f3e3179eeeccc3de06c351e25b14b0c893d
Jill Lidsky <jilllidsky15@gmail.com>
2020-06-15 21:54:53 -0400

    first commit

---
+++ tree-searching.js
@@ -0,0 +1,47 @@
+const Queue = require('./queue')
+
+class BinarySearchTree {
+    constructor(key = null, value = null, parent = null) {
+        this.key = key;
+        this.value = value;
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+    }
+
+    // start from a given node (usually the root) and traverse as far down as you can.
+    // when you reach a node, which has no children to visit or all nodes on its path have been visited, you start backtracking
+    dfs(values=[]) {
+        if (this.left) {
+            values = this.left.dfs(values);
+        }
+        values.push(this.value);
+
+        if (this.right) {
+            values = this.right.dfs(values);
+        }
+        return values;
+    }
+
+    // works across the rows of a tree
+    // top row is handled first, then second, and so on
+    bfs(tree, values = []) {
+        const queue = new Queue(); // Assuming a Queue is implemented (refer to previous lesson on Queue)
+        const node = tree.root;
+        queue.enqueue(node);
+        while (queue.length) {
+            const node = queue.dequeue(); //remove from the queue
+            values.push(node.value); // add that value from the queue to an array
+
+            if (node.left) {
+                queue.enqueue(node.left); //add left child to the queue
+            }
+
+            if (node.right) {
+                queue.enqueue(node.right); // add right child to the queue
+            }
+        }
+
+        return values;
+    }
+}
